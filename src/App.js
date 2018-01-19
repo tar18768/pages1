7c2efb8e70f7ac2990f19dc246fd0e8c9488d45c
tar18768 <tar18768@icloud.com>
2018-01-19 16:12:22 +0700

    changeee

--- src/App.js
+++ src/App.js
@@ -2,15 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import _ from 'lodash'
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
 const students =[
   {id:12, name:'mick', score: 23},
   {id:18, name:'yoyo', score: 27},
   {id:20, name:'yep', score: 22},
 ]
 
-const StudentLine = (props) => (
-  <div>{props.id} {props.name} = {props.score}</div>
+const StudentLine = ({id, name, score}) => (
+  <div>{id} {name} = {score}</div>
   
+)
+
+const StudentLink = ({id, name}) => (
+  <div><Link to = {'/student/${id}'}>{name}</Link></div>
 )
 
 class App extends Component {
